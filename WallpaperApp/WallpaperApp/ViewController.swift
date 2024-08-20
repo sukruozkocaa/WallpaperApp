8e4584fd286963e05754035431ee6202e3b5cdf4
Sukru Ozkoca <sukruozkocaa19@gmail.com>
2024-08-20 19:11:02 +0300

    Create wallpaper app project.

---
+++ WallpaperApp/WallpaperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallpaperApp
+//
+//  Created by Şükrü on 20.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
